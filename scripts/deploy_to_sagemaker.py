3097ac5316e3d19a7bb9cd5ef9ec6a885dccc250
Thomas <thomas.dreilich99@gmail.com>
2024-09-13 14:31:45 +0200

    Cleaned up deploy_to_sagemaker.py code.

--- scripts/deploy_to_sagemaker.py
+++ scripts/deploy_to_sagemaker.py
@@ -7,6 +7,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from dotenv import load_dotenv
 load_dotenv()
 
+# Set environment variables
+databucket = os.getenv('env_databucket')
+sagemakerARN = os.getenv('env_sagemakerARN')
+
 # Import boto3 and sagemaker for sagemaker jobs
 import boto3
 import sagemaker
@@ -24,32 +28,32 @@ s3.upload_file(file_name, bucket_name, file_name)
 
 # Assuming the SageMaker role
 sts_client = boto3.client('sts')
-
 assumed_role_object = sts_client.assume_role(
-    RoleArn=os.getenv('env_sagemakerARN'),
-    RoleSessionName="SageMakerSession"
+    RoleArn = sagemakerARN,
+    RoleSessionName = "SageMakerSession"
 )
 
+# Set credentials dictionary
 credentials = assumed_role_object['Credentials']
 
 # Create a SageMaker client using the assumed role credentials
 sagemaker_client = boto3.client(
     'sagemaker',
-    aws_access_key_id=credentials['AccessKeyId'],
-    aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken'],
-    region_name='eu-central-1'
+    aws_access_key_id = credentials['AccessKeyId'],
+    aws_secret_access_key = credentials['SecretAccessKey'],
+    aws_session_token = credentials['SessionToken'],
+    region_name = 'eu-central-1'
 )
 
 # Define the SKLearn Estimator
 sklearn_estimator = SKLearn(
-    entry_point='mlscripts/train.py',
-    role=os.getenv('env_sagemakerARN'),
-    instance_type='ml.m5.xlarge',
-    framework_version='0.23-1',
-    sagemaker_session=sagemaker.Session(),
-    hyperparameters={'alpha': 0.001}
+    entry_point = 'mlscripts/train.py',
+    role = sagemakerARN,
+    instance_type = 'ml.m5.xlarge',
+    framework_version = '0.23-1',
+    sagemaker_session = sagemaker.Session(),
+    hyperparameters = {'alpha': 0.001}
 )
 
 # Start the training job
-sklearn_estimator.fit({'train.py': os.getenv('env_databucket')})
+sklearn_estimator.fit({'train.py': databucket})
