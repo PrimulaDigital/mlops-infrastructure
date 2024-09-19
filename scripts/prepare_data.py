ff054c5661411854758d52d5fc2165aef8458784
Thomas <thomas.dreilich99@gmail.com>
2024-09-19 12:43:51 +0200

    Adjusted prepare_data.py to new util usage.

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,17 +1,16 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from data.Diabetes import load_data
 import pandas as pd
 
-def getCsv():
-    # Load the diabetes dataset
+def main():
+    '''
+    Load Diabetes Dataset. Create .csv file and return it.
+    '''
     diabetes = load_data()
 
-    # Create a DataFrame
     df = pd.DataFrame(data=diabetes.data, columns=diabetes.feature_names)
     df['target'] = diabetes.target
 
-    # Save the dataset as a CSV file and return it
     return df.to_csv('data/diabetes.csv', index=False)
+
+if __name__ == "__main__":
+    main()
