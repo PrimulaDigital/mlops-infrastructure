d0c1195ec88ddbd052b99f0c155149974f724be9
Thomas <thomas.dreilich99@gmail.com>
2024-09-26 11:57:31 +0200

    Defined some variables as locals.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,21 +18,6 @@ variable "volume_size_sagemaker" {
   type = number
 }
 
-variable s3_bucket_input_training_path {
-  type = string
-  description = "S3 path where training data is stored"
-}
-
-variable s3_bucket_output_models_path {
-  type = string
-  description = "S3 path were the output (trained models etc.) will be stored"
-}
-
-variable s3_object_training_data {
-  type = string
-  description = "S3 path where training data is stored"
-}
-
 variable "runtime" {
   type = string
   default = "python3.9.19"
