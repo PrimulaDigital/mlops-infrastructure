440141cff10e553dd3c2c1666f1121017ddfa90d
Thomas <thomas.dreilich99@gmail.com>
2024-10-11 12:41:21 +0200

    Added variables for subnets and vpc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,4 +9,15 @@ variable "project_name" {
   description = "Name of our project used in naming resources"
   default = "pd-sandbox01"
 }
+
+variable "vpc_id" {
+  type = string
+  description = "ID of pre built VPC"
+  default = "vpc-0276724cceddf86eb"
+}
  
+ variable "subnets" {
+  type = list
+  description = "List of available subnets in the pre built VPC"
+  default = ["subnet-073537da5ee37885a","subnet-07663b1b22ffaee50","subnet-00944e1eab3a4f1b4"]
+ }
