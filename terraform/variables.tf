8cee730efaab1b375569a44a6e69c33cb8097762
Thomas <thomas.dreilich99@gmail.com>
2024-10-11 13:54:44 +0200

    Added default instance type to variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,9 @@ variable "vpc_id" {
   description = "List of available subnets in the pre built VPC"
   default = ["subnet-073537da5ee37885a","subnet-07663b1b22ffaee50","subnet-00944e1eab3a4f1b4"]
  }
+
+ variable "instance_type" {
+  type = string
+  description = "Instance type for sagemaker jobs"
+  default = "ml.t3.medium"
+ }
