a08038d17ea0d3d18a251e5ace286735e0a983fe
Thomas <thomas.dreilich99@gmail.com>
2024-10-11 15:46:46 +0200

    Added architect and studio urls and sso

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,17 +13,35 @@ variable "project_name" {
 variable "vpc_id" {
   type = string
   description = "ID of pre built VPC"
-  default = "vpc-0276724cceddf86eb"
 }
  
  variable "subnets" {
   type = list
   description = "List of available subnets in the pre built VPC"
-  default = ["subnet-073537da5ee37885a","subnet-07663b1b22ffaee50","subnet-00944e1eab3a4f1b4"]
- }
+}
 
  variable "instance_type" {
   type = string
   description = "Instance type for sagemaker jobs"
-  default = "ml.t3.medium"
+  default = "ml.t3.large"
+ }
+
+ variable "architect_url" {
+  type = string
+  description = "URL for git repository that builds the infrastructure etc."
+ }
+
+ variable "studio_url" {
+  type = string
+  description = "URL for git repositroy that focuses on providing automation to data scientists"
+ }
+
+ variable "sso_architects" {
+  type = list
+  description = "List of SSO users that are architects"
+ }
+
+ variable "sso_scientists" {
+  type = list
+  description = "List of SSO users that are data scientists"
  }
